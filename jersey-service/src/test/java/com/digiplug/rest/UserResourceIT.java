32e7fd9c031032d8293fcb197b9e3f1515caa054
Cédric Dorot <cdorot@ippon.fr>
2013-12-03 11:16:58 +0100

    Clean integration test code

--- jersey-service/src/test/java/com/digiplug/rest/UserResourceIT.java
+++ jersey-service/src/test/java/com/digiplug/rest/UserResourceIT.java
@@ -18,65 +18,72 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserResourceIT {
 
+	static final String USER_RESOURCE_PATH = "/jersey-service-demo/webapi/users";
+
+	private CloseableHttpClient httpClient;
+
+	private CloseableHttpResponse response;
+
+	private URIBuilder userResourceURIBuilder;
+
+	public UserResourceIT() {
+		super();
+
+		this.httpClient = HttpClients.createDefault();
+		this.userResourceURIBuilder = new URIBuilder().setScheme("http").setHost("localhost").setPort(8080);
+	}
+
+	@Before
+	public void setup() {
+		this.response = null;
+	}
+
+	@After
+	public void teardown() throws IOException {
+		this.response.close();
+	}
+
 	@Test
 	public void getUser() throws URISyntaxException, IOException {
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-
-		URI uri = new URIBuilder().setScheme("http").setHost("localhost").setPort(8080)
-				.setPath("/jersey-service-demo/webapi/users/1").build();
+		URI uri = this.userResourceURIBuilder.setPath(USER_RESOURCE_PATH + "/1").build();
 
 		HttpGet httpGet = new HttpGet(uri);
 
-		CloseableHttpResponse response = httpClient.execute(httpGet);
+		this.response = this.httpClient.execute(httpGet);
 
-		try {
-			int statusCode = response.getStatusLine().getStatusCode();
+		int statusCode = response.getStatusLine().getStatusCode();
+		String content = IOUtils.toString(response.getEntity().getContent(), CharEncoding.UTF_8);
 
-			assertEquals(200, statusCode);
-
-			String content = IOUtils.toString(response.getEntity().getContent(), CharEncoding.UTF_8);
-
-			assertEquals("{\"firstname\":\"John\",\"id\":1,\"lastname\":\"Doe\"}", content);
-		} finally {
-			response.close();
-		}
+		assertEquals(200, statusCode);
+		assertEquals("{\"firstname\":\"John\",\"id\":1,\"lastname\":\"Doe\"}", content);
 	}
 
 	@Test
 	public void getUserAsJsonP() throws URISyntaxException, IOException {
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-
-		URI uri = new URIBuilder().setScheme("http").setHost("localhost").setPort(8080)
-				.setPath("/jersey-service-demo/webapi/users/jsonp/1").addParameter("jsonp", "mycallback").build();
+		URI uri = this.userResourceURIBuilder.setPath(USER_RESOURCE_PATH + "/jsonp/1")
+				.addParameter("jsonp", "mycallback").build();
 
 		HttpGet httpGet = new HttpGet(uri);
-
 		httpGet.setHeader("Accept", "application/javascript");
 
-		CloseableHttpResponse response = httpClient.execute(httpGet);
+		this.response = this.httpClient.execute(httpGet);
 
-		try {
-			int statusCode = response.getStatusLine().getStatusCode();
+		int statusCode = response.getStatusLine().getStatusCode();
+		String content = IOUtils.toString(response.getEntity().getContent(), CharEncoding.UTF_8);
 
-			assertEquals(200, statusCode);
-
-			String content = IOUtils.toString(response.getEntity().getContent(), CharEncoding.UTF_8);
-
-			assertEquals("mycallback({\"firstname\":\"John\",\"id\":1,\"lastname\":\"Doe\"})", content);
-		} finally {
-			response.close();
-		}
+		assertEquals(200, statusCode);
+		assertEquals("mycallback({\"firstname\":\"John\",\"id\":1,\"lastname\":\"Doe\"})", content);
 	}
 
 	@Test
 	public void postUser() throws URISyntaxException, IOException {
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		URI uri = new URIBuilder().setScheme("http").setHost("localhost").setPort(8080)
-				.setPath("/jersey-service-demo/webapi/users").build();
+		URI uri = this.userResourceURIBuilder.setPath(USER_RESOURCE_PATH).build();
 
 		HttpEntity entity = new StringEntity("{ \"firstname\":\"Jean\",\"lastname\":\"DUPONT\" }",
 				ContentType.APPLICATION_JSON);
@@ -84,41 +91,29 @@ public class UserResourceIT {
 
 		httpPost.setEntity(entity);
 
-		CloseableHttpResponse response = httpClient.execute(httpPost);
+		this.response = this.httpClient.execute(httpPost);
 
-		try {
-			int statusCode = response.getStatusLine().getStatusCode();
+		int statusCode = response.getStatusLine().getStatusCode();
+		String content = IOUtils.toString(response.getEntity().getContent(), CharEncoding.UTF_8);
 
-			assertEquals(201, statusCode);
-
-			String content = IOUtils.toString(response.getEntity().getContent(), CharEncoding.UTF_8);
-
-			assertTrue(content.matches("\\{\"firstname\":\"Jean\",\"id\":\\d+,\"lastname\":\"DUPONT\"\\}"));
-		} finally {
-			response.close();
-		}
+		assertEquals(201, statusCode);
+		assertTrue(content.matches("\\{\"firstname\":\"Jean\",\"id\":\\d+,\"lastname\":\"DUPONT\"\\}"));
 	}
 
 	@Test
 	public void postInvalidUser() throws URISyntaxException, IOException {
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		URI uri = new URIBuilder().setScheme("http").setHost("localhost").setPort(8080)
-				.setPath("/jersey-service-demo/webapi/users").build();
+		URI uri = this.userResourceURIBuilder.setPath(USER_RESOURCE_PATH).build();
 
 		HttpEntity entity = new StringEntity("{ \"lastname\":\"DUPONT\" }", ContentType.APPLICATION_JSON);
 		HttpPost httpPost = new HttpPost(uri);
 
 		httpPost.setEntity(entity);
 
-		CloseableHttpResponse response = httpClient.execute(httpPost);
+		this.response = this.httpClient.execute(httpPost);
 
-		try {
-			int statusCode = response.getStatusLine().getStatusCode();
+		int statusCode = response.getStatusLine().getStatusCode();
 
-			assertEquals(400, statusCode);
-		} finally {
-			response.close();
-		}
+		assertEquals(400, statusCode);
 	}
 
 }
